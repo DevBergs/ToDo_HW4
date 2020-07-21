c7c1b12d95a12552c02af26feb6241e857f2e334
DevBergs <karlsbergsaivis@gmail.com>
2020-07-22 01:08:10 +0300

    Prinful project html, css, JS

--- js/app.js
+++ js/app.js
@@ -1,37 +1,52 @@
-
+// Creating close button
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  var txt = document.createTextNode("Dzēst");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
+// Create edit button
+var myEditlist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myEditlist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("Labot");
+  span.className = "edit";
+  span.appendChild(txt);
+  myEditlist[i].appendChild(span);
+}
+
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
-  }
+  };
 }
 
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
 
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
+  if (inputValue === "") {
+    alert("Neatstāj tukšu!");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
@@ -44,11 +59,9 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
-    }
+    };
   }
 }
-
-
